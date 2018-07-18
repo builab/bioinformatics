d34832f4a6c003340226569341f8bcfeb788545f
builab <huy.bui@mcgill.ca>
2018-07-18 11:06:09 -0400

    Update retrieve_multi_uniprot_fasta.sh

--- retrieve_multi_uniprot_fasta.sh
+++ retrieve_multi_uniprot_fasta.sh
@@ -3,7 +3,7 @@
 # Usage: sh retrieve_multi_uniprot_fasta.sh < uniprotID_list.txt
 while read uniprotId; do
 	echo $uniprotId
-	echo curl http://www.uniprot.org/uniprot/${uniprotId}.fasta
-	curl http://www.uniprot.org/uniprot/${uniprotId}.fasta > ${uniprotId}.fasta
+	echo curl https://www.uniprot.org/uniprot/${uniprotId}.fasta
+	curl https://www.uniprot.org/uniprot/${uniprotId}.fasta > ${uniprotId}.fasta
 done
 
