fcc1e792c1c129ff4884d5f0eb8ed7dec67fcdad
builab <huy.bui@mcgill.ca>
2018-07-18 10:56:40 -0400

    Update retrieve_multi_uniprot_fasta.sh

--- retrieve_multi_uniprot_fasta.sh
+++ retrieve_multi_uniprot_fasta.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 # Biopython Retrieving Seq from Uniprot ID
-
+# Usage: sh retrieve_multi_uniprot_fasta.sh < uniprotID_list.txt
 while read uniprotId; do
 	echo $uniprotId
 	echo curl http://www.uniprot.org/uniprot/${uniprotId}.fasta
 	curl http://www.uniprot.org/uniprot/${uniprotId}.fasta > ${uniprotId}.fasta
-done < seq_list_fixed.txt
+done
 
